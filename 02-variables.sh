99f7bb2bf640b94797aef1d02fee3eee0a0fce21
RajinikarBiram <rajinikar1445@gmail.com>
2024-04-17 11:58:37 -0700

    printing conersation

---
+++ 02-variables.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+ 
+echo "Ramesh :: Hello Suresh, how are you?"
+echo "Suresh :: Hi Ramesh, I  am fine, how are you?"
+echo "Ramesh :: I am fine too, how is your work?"
+echo "Suresh :: not bad, I am thinking to learn DevOps"
