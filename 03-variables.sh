dd0a5298c834bf09184ea44a47d2479eea86a0ab
RajinikarBiram <rajinikar1445@gmail.com>
2024-04-17 12:05:44 -0700

    printing variables

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#declaring variable
+PERSON1=Ramesh  #no space in between =and value
+PERSON2=Suresh
+#referring variable
+echo "$PERSON1 :: Hello $PERSON2, how are you?"
+echo "$PERSON2 :: Hi $PERSON1, I  am fine, how are you?"
+echo "$PERSON1 :: I am fine too, how is your work?"
+echo "$PERSON2 :: not bad, I am thinking to learn DevOps"
+
+# $ Shell will consider it as variable 
