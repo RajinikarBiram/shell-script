68e6765f25e6fefc1e343c25ce4bc2e36e61d4a7
RajinikarBiram <rajinikar1445@gmail.com>
2024-04-17 12:10:54 -0700

    printing variables

--- 04-variables.sh
+++ 04-variables.sh
@@ -2,8 +2,11 @@
 
 PERSON1=$1  
 PERSON2=$2
+PERSON3=$3
 
 echo "$PERSON1 :: Hello $PERSON2, how are you?"
 echo "$PERSON2 :: Hi $PERSON1, I  am fine, how are you?"
 echo "$PERSON1 :: I am fine too, how is your work?"
 echo "$PERSON2 :: not bad, I am thinking to learn DevOps"
+echo "$PERSON3 :: hey, sorry to interupt, I need some help guys"
+
