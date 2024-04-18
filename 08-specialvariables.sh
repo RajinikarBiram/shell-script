2e2eaf60415a7e2e78f1d1164a4afa6637d601e0
RajinikarBiram <rajinikar1445@gmail.com>
2024-04-18 11:45:15 -0700

    printing special variable type

---
+++ 08-specialvariables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo " All variables : $@"
+echo " Number of variables passed : $#"
+echo " Script name : $0"
+echo " Current working directory : $PWD"
+echo " Home directory of current user : $HOME"
+echo " Which user is running the script : $USER"
+echo " Host name : $HOSTNAME"
+echo " Process ID of the current shell script : $$"
