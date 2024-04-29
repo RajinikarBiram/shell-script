d9d426e932f968071b2c33da1c622a66636110b2
RajinikarBiram <rajinikar1445@gmail.com>
2024-04-29 10:58:11 -0700

    installing mysql using functions

---
+++ 11-functions.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPTNAME=$(echo $0 | cut -d "." f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+VALIDATE(){
+    if[$1 -ne 0]
+    then
+        echo "$2..FAILURE"
+        exit 1
+    else
+        echo "$2..SUCCESS"
+    fi
+}
+if [$USERID -ne 0]
+then 
+   echo "Please run this script with root access"
+   exit 1
+else
+   echo " You are a super user"
+fi 
+dnf install mysql -y &>>$LOGFILE
+VALIDATE $? "Installing MYSQL"
