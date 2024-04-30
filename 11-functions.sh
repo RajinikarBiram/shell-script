9b943bcbab836090b7daa774604fa30a04ce536d
RajinikarBiram <rajinikar1445@gmail.com>
2024-04-29 20:08:42 -0700

    colors

--- 11-functions.sh
+++ 11-functions.sh
@@ -4,13 +4,13 @@ TIMESTAMP=$(date +%F-%H-%M-%S)
 SCRIPT_NAME=$(echo $0 | cut -d "." f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 VALIDATE(){
-    if[$1 -ne 0]
-    then
-        echo "$2..FAILURE"
-        exit 1
-    else
-        echo "$2..SUCCESS"
-    fi
+if[$1 -ne 0]
+then
+ echo "$2..FAILURE"
+ exit 1
+else
+ echo "$2..SUCCESS"
+fi
 }
 if [$USERID -ne 0]
 then 
