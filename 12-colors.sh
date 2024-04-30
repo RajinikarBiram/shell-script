83d755befe72c8bb3ef65d7e7c60b15946220315
RajinikarBiram <rajinikar1445@gmail.com>
2024-04-29 17:22:26 -0700

    colors

--- 12-colors.sh
+++ 12-colors.sh
@@ -8,12 +8,12 @@ G="\e[32m"
 N="\e[0m"
 VALIDATE(){
     if[$1 -ne 0]
-     then
-        echo -e "$2..$R FAILURE $N"
-        exit 1
+    then
+    echo -e "$2..$R FAILURE $N"
+    exit 1
     else
-        echo -e "$2..$G SUCCESS $N"
-    fi
+    echo -e "$2..$G SUCCESS $N"
+	fi
 }
 if [$USERID -ne 0]
 then 
