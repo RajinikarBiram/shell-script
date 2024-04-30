9936ee3b3071552ed78381a6d374f5c8a581e077
RajinikarBiram <rajinikar1445@gmail.com>
2024-04-29 20:16:28 -0700

    testing

--- 11-functions.sh
+++ 11-functions.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
+
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." f1)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+
 VALIDATE(){
-   if[$1 -ne 0]
+   if [ $1 -ne 0 ]
    then
-       echo "$2..FAILURE"
-       exit 1
-   else
-       echo "$2..SUCCESS"
-   fi
+        echo "$2...FAILURE"
+        exit 1
+    else
+        echo "$2...SUCCESS"
+    fi
 }
-if [$USERID -ne 0]
-then 
-    echo "Please run this script with root access"
-    exit 1
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root access."
+    exit 1 # manually exit if error comes.
 else
-    echo " You are a super user"
-fi 
+    echo "You are super user."
+fi
+
 dnf install mysql -y &>>$LOGFILE
-VALIDATE $? "Installing MYSQL"
+VALIDATE $? "Installing MySQL"
