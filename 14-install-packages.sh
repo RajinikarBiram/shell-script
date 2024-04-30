ad1fe92c48e4da39fcdcbfb6d2f3f0b184633e51
RajinikarBiram <rajinikar1445@gmail.com>
2024-04-29 20:24:40 -0700

    testing

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -7,15 +7,17 @@ R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
+
 VALIDATE(){
     if[$1 -ne 0]
     then
-        echo -e "$2..$R FAILURE $N"
+        echo -e "$2...$R FAILURE $N"
         exit 1
     else
-        echo -e "$2..$G SUCCESS $N"
+        echo -e "$2...$G SUCCESS $N"
     fi
 }
+
 if [$USERID -ne 0]
 then 
    echo "Please run this script with root access"
@@ -24,6 +26,7 @@ else
    echo " You are a super user"
 fi 
 #for many packages to install
+
 for i in $@
 do 
   echo "Package to install : $i"
