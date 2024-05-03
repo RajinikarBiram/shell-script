75bcab687bd34d336ef843a31f82edd36a2e2251
RajinikarBiram <rajinikar1445@gmail.com>
2024-05-03 15:28:15 -0700

    functions

---
+++ 17-set.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ 
+set -e
+
+failure(){
+    echo "Error occured at line number: $1, error command: $2"
+}
+
+trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
+
+USERID=$(id -u)
+if [$USERID -ne 0]
+then 
+    echo "Please run this script with root access"
+    exit 1 #manually exit if error occurs
+else
+    echo " You are a super user"
+fi
+
+dnf install mysql -y
+
+dnf install git -y
+
+echo "is script proceeding?"
