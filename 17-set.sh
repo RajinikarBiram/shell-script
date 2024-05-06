a4e90db49249cf3a504dd7c88e377f888eb3856e
RajinikarBiram <rajinikar1445@gmail.com>
2024-05-06 14:16:57 -0700

    functions

--- 17-set.sh
+++ 17-set.sh
@@ -9,6 +9,7 @@ failure(){
 trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 
 USERID=$(id -u)
+
 if [$USERID -ne 0]
 then 
     echo "Please run this script with root access"
@@ -18,7 +19,6 @@ else
 fi
 
 dnf install mysql -y
-
 dnf install git -y
 
 echo "is script proceeding?"
